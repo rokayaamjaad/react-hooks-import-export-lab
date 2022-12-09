47eb9c7f0690fda188b167430c8ab419e76f4f2e
rokayaamjaad <Rukayaamged733@gmail.com>
2022-12-10 00:34:08 +0300

    finsh

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import NavBar from "../components/NavBar.js";
+import Home from "../components/Home.js";
+import About from "../components/About.js";
+
 
 function App() {
   return (
